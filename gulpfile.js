04b76a2b81139f6bd31ec1392ad4ac136745e7cf
Taulim <taulimtank@gmail.com>
2018-12-22 15:02:44 +0000

    New structure to the project and gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,54 @@
 var gulp = require('gulp');
-//var gulpConcat = require('gulp-concat');
+var concat = require('gulp-concat');
+var coffee = require('gulp-coffee');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
 
-gulp.task('hello', function(pFnDone) {
-	console.log('Hello World');
+gulp.task('sass', function(pFnDone) {
+	!(gulp.src('app/scss/**/*.scss')
+		.pipe(sass())
+		.pipe(concat('scssStyles.css'))
+		.pipe(gulp.dest('app/css'))
+	);
+	pFnDone();
+});
+
+gulp.task('css', function(pFnDone) {
+	!(gulp.src('app/css/**/*.css')
+		.pipe(concat('styles.css'))
+		.pipe(gulp.dest('dist/css'))
+	);
+	pFnDone();
+});
+
+gulp.task('coffee', function(pFnDone) {
+	!(gulp.src('app/coffee/**/*.coffee')
+		.pipe(coffee())
+		.pipe(concat('coffeeScripts.js'))
+		.pipe(gulp.dest('app/js'))
+	);
 	pFnDone();
 });
 
 gulp.task('js', function(pFnDone) {
 	!(gulp.src('app/js/*.js')
-		//.pipe(gulpConcat('scripts.js'))
+		.pipe(concat('scripts.js'))
+		.pipe(uglify())
 		.pipe(gulp.dest('dist/js'))
 	);
 	pFnDone();
-})
+});
 
-gulp.task('default', gulp.series('js','hello'));
+gulp.task('watch', function(pFnDone) {
+	gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
+	gulp.watch('app/css/**/*.css', gulp.series('css'));
+	gulp.watch('app/coffee/**/*.coffee', gulp.series('coffee'));
+	gulp.watch('app/js/**/*.js', gulp.series('js'));
+	//pFnDone();
+});
+
+gulp.task('build', gulp.series(gulp.parallel(gulp.series('sass','css'), gulp.series('coffee', 'js'))));
+
+gulp.task('dev', gulp.series('build', 'watch'));
+
+gulp.task('default', gulp.series('build'));
